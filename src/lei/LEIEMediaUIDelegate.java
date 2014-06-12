1aacea8018c4f2dc8de912913e525c400f00375b
Kyiys <rjcosta2@hotmail.com>
2014-06-12 22:01:25 +0100

    Almost done with LEIEMediaUIDelegate, Godfather unnavailable, only missing setObservers I guess

--- src/lei/LEIEMediaUIDelegate.java
+++ src/lei/LEIEMediaUIDelegate.java
@@ -1,9 +1,17 @@
 package lei;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Observer;
 
-import controller.delegates.EMediumUIDelegate;
+import services.viewer.NoSuchPageException;
 import model.EMedium;
+import model.EMediumPropertiesData;
+import model.lendables.Lendable;
+import model.rentals.RentalFactory;
+import model.rentals.BookRental;
+import controller.delegates.EMediumUIDelegate;
 
 /**
  * The e-media viewer ui delegate default implementation
@@ -13,55 +21,81 @@ import model.EMedium;
  */
 public class LEIEMediaUIDelegate extends EMediumUIDelegate {
 	
-	//TODO: program me!
+	private EMedium eMedia;
+	private Collection<Observer> observers;
+	private int lastpagevisited;
+	private BookRental rental;
 	
 	public void setEMedia (EMedium eMedia) {
+		this.eMedia=eMedia;
+		// a ultima pagina a aceder foi a primeira
+		this.observers= new ArrayList<Observer>();
+		this.rental= new BookRental((new Lendable(eMedia.getType(), eMedia.getEMediumProperties())));
 	}
 	
 	@Override
 	public void setObservers() {
+//		Don't know what to do
 	}
 
 
 	@Override
 	public void deleteObservers() {
+		for(Observer ob : observers)
+			eMedia.deleteObserver(ob);
+		observers.removeAll(observers);
 	}
 
 
 	@Override
 	public void setLastPageVisited(int pageNum) {
+				lastpagevisited=pageNum;
 	}
 
 
 	@Override
 	public boolean isBookmarked(int pageNum) {
-		return false;
+		try {
+			return rental.isBookmarked(pageNum);
+		} catch (NoSuchPageException e) {
+			return false;
+		}
 	}
 
 
 	@Override
 	public int getLastPageVisited() {
-		return 1;
+		return lastpagevisited;
 	}
 
 	@Override
 	public File getEMediaFile() {
-		return null;
+		return eMedia.getFile();
 	}
 
 
 	@Override
 	public boolean hasAnnotations(int pageNum) {
-		return false;
+		try {
+			return rental.hasAnnotations(pageNum);
+		} catch (NoSuchPageException e) {
+			System.out.println("Pagina não pertence ao livro");
+			return false;
+		}
 	}
 
 
 	@Override
 	public void toggleBookmark(int pageNum) {
+		try {
+			rental.toggleBookmark(pageNum);
+		} catch (NoSuchPageException e) {
+			System.out.println("Pagina não pertence ao livro");
+		}
 	}
 	
 	@Override
 	public EMedium getEMedia() {
-		return null;
+		return eMedia;
 	}
 }
