906c2de6e31c83c330a5e27b2e432838656e0b9f
Kazemaru <pdfantunes@gmail.com>
2014-06-13 02:39:16 +0100

    Partially programmed LEIEMediumMetadataUIDelegate.java

--- src/lei/LEIEMediumMetadataUIDelegate.java
+++ src/lei/LEIEMediumMetadataUIDelegate.java
@@ -1,10 +1,13 @@
 package lei;
 
-import java.util.LinkedList;
+import java.util.Iterator;
 import java.util.Observable;
 
-import controller.delegates.EMediumMetadataUIDelegate;
 import model.EMedium;
+import model.lendables.Lendable;
+import model.rentals.BookRental;
+import services.viewer.NoSuchPageException;
+import controller.delegates.EMediumMetadataUIDelegate;
 
 /**
  * The document's metadata ui delegate default implementation
@@ -14,49 +17,72 @@ import model.EMedium;
  */
 public class LEIEMediumMetadataUIDelegate extends EMediumMetadataUIDelegate {
 	
-	//TODO: program me!
+	private BookRental rental;
 	
 	public void setEMedium (EMedium document) {
+		this.rental= new BookRental((new Lendable(document.getType(), document.getEMediumProperties())));
+
 	}
 	
 	@Override
 	public void deleteObservers() {
+		//TODO
 	}
 
 	@Override
 	public Iterable<Integer> getDocumentBookmarks() {
-		return new LinkedList<Integer>();
+		return this.rental.getBookmarks();
 	}
 
 	@Override
 	public Iterable<String> getPageAnnotations(int pageNum) {
-		return new LinkedList<String>();
+		return this.rental.getAnnotations();
 	}
 
 	@Override
 	public String getDocumentTitle() {
-		return "title";
+		return this.rental.getTitle();
 	}
 
 	@Override
 	public void addAnnotation(int pageNum, String text) {
+		try {
+			this.rental.addAnnotation(pageNum, text);
+		} catch (NoSuchPageException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void removeAnnotation(int pageNum, int annotNum) {
+		try {
+			this.rental.removeAnnotation(pageNum, annotNum);
+		} catch (NoSuchPageException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void toggleBookmark(int pageNum) {
+		try {
+			this.rental.toggleBookmark(pageNum);
+		} catch (NoSuchPageException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public String getAnnotationText(int pageNum, int annotNum) {
-		return "";
+		return this.getPageAnnotations(pageNum).; //TODO TASUKETE
 	}
 
 	@Override
 	public void update(Observable arg0, Object hint) {
+		//TODO
+		
 	}
 
 }
